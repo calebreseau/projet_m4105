05b6fe930e2ab0da5c620508d977b332d560b01d
calebreseau <arthur.labalec@gmail.com>
2021-03-26 14:19:44 +0100

    test

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 var express=require('express');
 var app=express();
 
+const beers = require('./open-beer-database.json')
+
+app.use(express.json())
+app.set('json spaces', 40);
+
+
 app.get('/',function(req,res){
 
     res.send('Hello World!');
 
 });
 
-app.get('/test',function(req,res){
+app.get('/api/beers/',function(req,res){
 
-    res.send('test');
+    res.status(200).json(beers);
+
+});
+
+app.get('/api/beers/:brewery_id',function(req,res){
+
+    console.log('brewery id');
+    const id = parseInt(req.params.brewery_id);
+    console.log('id:'+id)
+    const beer = beers.find(beers => beers.brewery_id === id);
+    console.log(beer);
+    res.status(200).json(beer);
 
 });
 
